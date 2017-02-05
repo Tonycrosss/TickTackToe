5c110be7e5955d54d35caa5344fef92bfeebd8c4
TonyCross <starovoitov.anton@gmail.com>
2017-02-05 15:36:29 +0500

    -board creation -board display

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 
+squares_number = 9
 
-board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+def new_board():
+    board=[]
+    for square in range(squares_number):
+        board.append(square)
+    return board
 
-for k in board:
-    print(k)
+
+def display_board(board):
+    print("\n\t",board[0],"|",board[1],"|", board[2])
+    print("\t","----------")
+    print("\n\t",board[3],"|",board[4],"|", board[5])
+    print("\t","----------")
+    print("\n\t",board[6],"|",board[7],"|", board[8], "\n")
+
+
+board = new_board()
+display_board(board)
