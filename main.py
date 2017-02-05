875430f15a294c08b6a5c50205d6ed99ae1db48f
TonyCross <starovoitov.anton@gmail.com>
2017-02-05 15:37:29 +0500

    pep8

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-
 squares_number = 9
 
+
 def new_board():
-    board=[]
+    board = []
     for square in range(squares_number):
         board.append(square)
     return board
 
 
 def display_board(board):
-    print("\n\t",board[0],"|",board[1],"|", board[2])
-    print("\t","----------")
-    print("\n\t",board[3],"|",board[4],"|", board[5])
-    print("\t","----------")
-    print("\n\t",board[6],"|",board[7],"|", board[8], "\n")
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "----------")
+    print("\n\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "----------")
+    print("\n\t", board[6], "|", board[7], "|", board[8], "\n")
 
 
 board = new_board()
